692bbaa4908ef6b6dd0acfa0c685408f5131fc78
phootip <phootip.t@gmail.com>
2019-11-13 18:24:15 +0700

    able to fetch and filter

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Thanks Dev Coffee! :) 
 // https://www.youtube.com/watch?v=VPUdtEf3oXI
-
 new Vue({
   el: '#app',
   data: function() {
@@ -14,28 +13,30 @@ new Vue({
       players: [
         { name: 'Charles Wilson', sport: 'Baseball', views: 312},
         { name: 'Tony Domo', sport: 'Football', views: 23121 },
-        { name: 'Rump Baker', sport: 'Basketball', views: 231},
-        { name: 'Brad Pitt', sport: 'Acting', views: 443},
-        { name: 'Kermit the Frog', sport: 'Frogball', views: 403},
-        { name: 'Taylor Swift', sport: 'Singing', views: 8843}
-      ]
+      ],
+      items: [],
+      username: 'sunsetwithbubbles',
     }
   },
   computed: {
-    getPlayers() {
-      
-      var players = this.players.filter((player) => {
-          return player.name.toLowerCase().includes(this.filter.toLowerCase());
-        });
-      
-      if (this.sort == 'views') {
-        return players.sort(function(a, b) {
-          return b.views - a.views
-        });
-      } else {
-        return players;
+    getItems() {
+      var items = this.items.filter((item) => {
+        return item.caption.toLowerCase().includes(this.filter.toLowerCase());
+      });
+    
+      return items
+    }
+  },
+  methods: {
+    async publicFetch () {
+      console.log("start publicFetch...")
+      try {
+        this.items = await resolveItems(this.username, this.item)
+        console.log(this.items)
+      } catch (e) {
+        console.error('Fetching Instagram photos failed', e)
       }
-      
+      console.log("end publicFetch")
     }
   }
 })
